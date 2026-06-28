d647fccb396e3d678a89a0330f7c4ca719c2e863
girija-sonawane <sonawanegirija0@gmail.com>
2026-06-28 22:06:54 +0530

    Created first Java programs and learned basic structure

---
+++ Goal.java
@@ -0,0 +1,10 @@
+package learning;
+
+public class Goal {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("I will become a java full stack developer");
+	}
+
+}
---
+++ Introduction.java
@@ -0,0 +1,13 @@
+package learning;
+
+public class Introduction {
+	
+	public static void main(String []args) {
+		
+		System.out.println("Hello, My name is Girija.\r\n"
+				+ "I am learning Java.\r\n"
+				+ "I love to code in java.");
+		
+	}
+
+}
